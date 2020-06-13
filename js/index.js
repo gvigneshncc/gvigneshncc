56969386bb478dd5e2d55cd9d91f82b6d2d8b43d
gvigneshncc <66474613+gvigneshncc@users.noreply.github.com>
2020-06-13 16:14:28 +0530

    Focus on page load

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 import { createBoard } from "./board.js";
 import { closeCard } from "./card.js";
 
+window.addEventListener("load", () => {
+  document.getElementById("board-name").focus();
+});
+
 document.getElementById("create-board").addEventListener("click", createBoard);
 document.getElementById("close-card").addEventListener("click", closeCard);
